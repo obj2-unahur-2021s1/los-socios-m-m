8a1886f24be610b2824322d1032b679ba692acc6
unknown <tomasmiranda98@hotmail.com>
2021-05-01 20:38:48 -0300

    se plantea caso inicial para cliente

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,53 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Cliente(var estadoQueManeja: EstadoDeAnimo, var plataEnMano: Int, var residencia: LugarDondeVive  ) {
+}
+abstract class EstadoDeAnimo{
+    abstract fun cuantoDejaDePropina()
+}
+object estaEnojado : EstadoDeAnimo() {
+    override fun cuantoDejaDePropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+object estaFeliz : EstadoDeAnimo(){
+    override fun cuantoDejaDePropina() {
+        TODO("Not yet implemented")
+    }
+}
+object estaIndiferente : EstadoDeAnimo(){
+    override fun cuantoDejaDePropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+object estaResfriado : EstadoDeAnimo(){
+    override fun cuantoDejaDePropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+abstract class LugarDondeVive{
+    abstract fun calcularBono()
+}
+object lasRosas: LugarDondeVive(){
+    override fun calcularBono() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+object lasLauchas : LugarDondeVive(){
+    override fun calcularBono() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+object barrioVerde : LugarDondeVive(){
+    override fun calcularBono() {
+        TODO("Not yet implemented")
+    }
+
 }
