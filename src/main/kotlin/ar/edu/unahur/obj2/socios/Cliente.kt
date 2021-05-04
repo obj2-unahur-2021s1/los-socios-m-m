4ae9da82a9f4310acbf7749bfc760705fc8e08cb
unknown <tomasmiranda98@hotmail.com>
2021-05-04 16:02:11 -0300

    se agrega el efecto de la ciudad sobre la propina, y tests

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,8 +1,10 @@
 package ar.edu.unahur.obj2.socios
 
 class Cliente(var estadoQueManeja: EstadoDeAnimo, var plataEnBolsillo: Double, var residencia: LugarDondeVive) {
-    fun darPropina(valorPedido: Double) = estadoQueManeja.cuantoDejaDePropina(plataEnBolsillo, valorPedido)
-    }
+    fun propinaSinTenerEnCuentaLugar(valorPedido: Double) = estadoQueManeja.cuantoDejaDePropina(plataEnBolsillo, valorPedido)
+
+    fun propinaConEfectoDelLugar(valorPedido: Double) = residencia.modificacionDePropinaPorLugar(this.propinaSinTenerEnCuentaLugar(valorPedido))
+}
 
 abstract class EstadoDeAnimo{
     abstract fun cuantoDejaDePropina(plataEnBolsillo: Double, valorPedido: Double) : Double
@@ -14,49 +16,54 @@ object estaEnojado : EstadoDeAnimo() {
     }
 }
 
-// me queda la duda si la plata en el bolsillo es, como mínimo, el valor del pedido...
+// me queda la duda si la plata en el bolsillo es, como mínimo, el valor del pedido... tiene sentido
 object estaIndiferente : EstadoDeAnimo() {
     override fun cuantoDejaDePropina(plataEnBolsillo: Double, valorPedido: Double) : Double {
         return plataEnBolsillo
     }
 }
 
-
-/*
 object estaFeliz : EstadoDeAnimo(){
-    override fun cuantoDejaDePropina() {
-        TODO("Not yet implemented")
+    override fun cuantoDejaDePropina(plataEnBolsillo: Double, valorPedido: Double): Double {
+        return valorPedido*1.25
     }
+
 }
 
 
 object estaResfriado : EstadoDeAnimo(){
-    override fun cuantoDejaDePropina() {
-        TODO("Not yet implemented")
+    override fun cuantoDejaDePropina(plataEnBolsillo: Double, valorPedido: Double): Double {
+        return valorPedido
     }
+
 }
-*/
+
 
 abstract class LugarDondeVive{
-    abstract fun calcularBono()
+    abstract fun modificacionDePropinaPorLugar(propina: Double) : Double
 }
 object lasRosas: LugarDondeVive(){
-    override fun calcularBono() {
-        TODO("Not yet implemented")
+    override fun  modificacionDePropinaPorLugar(propina: Double): Double {
+        return propina + 50
     }
 
 }
 
 object lasLauchas : LugarDondeVive(){
-    override fun calcularBono() {
-        TODO("Not yet implemented")
+    override fun modificacionDePropinaPorLugar(propina: Double) : Double {
+       return propina/2
     }
 
 }
 
 object barrioVerde : LugarDondeVive(){
-    override fun calcularBono() {
-        TODO("Not yet implemented")
-    }
+    override fun  modificacionDePropinaPorLugar(propina: Double) : Double {
+       //no me quedo claro lo de minimo de 200
+        return propina + 200    }
 
+}
+object lasTorres : LugarDondeVive() {
+    override fun  modificacionDePropinaPorLugar(propina: Double) : Double {
+        return propina
+    }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -5,10 +5,18 @@ import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
   describe("Un/a cliente") {
-    val clienteEnojadoDeLasRosas = Cliente(estaEnojado,25.0, lasRosas)
-
+    val clienteEnojadoDeLasTorres = Cliente(estaEnojado, 25.0, lasTorres)
+    val clienteResfriadaDeLasLauchas = Cliente(estaResfriado, 50.0, lasLauchas)
     it("no entrega propina") {
-      clienteEnojadoDeLasRosas.darPropina(158.0).equals(0)
+      clienteEnojadoDeLasTorres.propinaSinTenerEnCuentaLugar(158.0).equals(0)
+    }
+
+    it("la propina, por efecto del lugar, deberia ser la mitad del pedido") {
+      clienteResfriadaDeLasLauchas.propinaConEfectoDelLugar(1000.0).equals(500)
+
+    }
+    it("el lugar las torres no afecta el precio del pedido"){
+      lasTorres.modificacionDePropinaPorLugar(500.0).equals(500)
     }
   }
 })
